843878bbd629fd0a0db1c13a6a8e0c5b00c4f873
Spyros Seimenis <sespiros@gmail.com>
2014-06-29 22:19:38 +0300

    LIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIINTS

--- views/js/dashboard.js
+++ views/js/dashboard.js
@@ -1,41 +1,45 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert, google*/
 // slightly modified myreports.js fix for map in dynamic content
 // http://goo.gl/A2rh2I
 var map = [];
 
-$(document).ready(function(){
+$(document).ready(function () {
+    "use strict";
 	var totals;
-	$('#content-open').load('resources/openReports.php', function() {
-		totals = parseInt($('#content-open').find('#totalopen').html());
+	$('#content-open').load('resources/openReports.php', function () {
+		totals = parseInt($('#content-open').find('#totalopen').html(), 10);
 
 		// init bootpag
 		$('#page-selection-open').bootpag({
 			total: totals,
 			page: 1,
 			maxVisible: 4
-		}).on("page", function(event, num){
-				$("#content-open").load('resources/openReports.php', {page: num});
+		}).on("page", function (event, num) {
+            $("#content-open").load('resources/openReports.php', {page: num});
 		});
 
 		$('.report-modal').on('shown.bs.modal', function (e) {
-			var i = e.currentTarget.id;
-			if (map[i] == undefined){
+			var i = e.currentTarget.id, that,
+                lat, long, mapOptions, marker, center;
+			if (map[i] === undefined) {
 				// find coordinates
-				var that = $(this);
-				var lat = that.find('span[data-lat]').data('lat');
-				var long = that.find('span[data-long]').data('long');
+				that = $(this);
+				lat = that.find('span[data-lat]').data('lat');
+				long = that.find('span[data-long]').data('long');
 
-				var mapOptions = {
+				mapOptions = {
 					center: new google.maps.LatLng(lat, long),
 					zoom: 16,
 					disableDefaultUI: true
 				};
-				map[i] = new google.maps.Map(document.getElementById('map'+i), mapOptions);
-				var marker = new google.maps.Marker({
+				map[i] = new google.maps.Map(document.getElementById('map' + i), mapOptions);
+				marker = new google.maps.Marker({
 					position: mapOptions.center,
 					map: map[i]
 				});
-			}else{
-				var center = map[i].getCenter();
+			} else {
+				center = map[i].getCenter();
 				google.maps.event.trigger(map[i], "resize");
 				map[i].setCenter(center);
 			}
@@ -46,16 +50,16 @@ $(document).ready(function(){
                 }
 	});
 
-	$('#content-closed').load('resources/closedReports.php', function() {
-		totals = parseInt($('#content-closed').find('#totalclosed').html());
+	$('#content-closed').load('resources/closedReports.php', function () {
+		totals = parseInt($('#content-closed').find('#totalclosed').html(), 10);
 
 		 //init bootpag
 		$('#page-selection-closed').bootpag({
 			total: totals,
 			page: 1,
 			maxVisible: 4
-		}).on("page", function(event, num){
-				$("#content-closed").load('resources/closedReports.php', {page: num});
+		}).on("page", function (event, num) {
+            $("#content-closed").load('resources/closedReports.php', {page: num});
 		});
 
                 if ($(this).find('tbody tr').length === 1) {
@@ -63,7 +67,7 @@ $(document).ready(function(){
                 }
 	});
 
-	$(document).on("click", ".closebtn", function() {
+	$(document).on("click", ".closebtn", function () {
 		/* Loipon o logos gia tin parakatw asxhmia einai o eksis:
 		 * otan epestrefa ta dynamika modals meta tin epilogi selidas
 		 * den mporousa na parw to form pou eixa ekei, kai oti tropos
@@ -72,52 +76,51 @@ $(document).ready(function(){
 		 * apo ta elements kai kataskeuazw ena form to opoio ginetai
 		 * non-ajax post sto dashboard.php.
 		 * */
-		var modalform = $(this).parent().parent();
-		var id = modalform.find('span[data-id]').data('id');
-		var comment = modalform.find('textarea').val();
-		var post_data = { report_id: id, comment: comment, markClosed: 1 };
+		var modalform = $(this).parent().parent(),
+		    id = modalform.find('span[data-id]').data('id'),
+            comment = modalform.find('textarea').val(),
+            post_data = { report_id: id, comment: comment, markClosed: 1 };
 
 		$('<form method="post" action="dashboard.php" role="form">' +
 			'<input name="report_id" value="' + id + '">' +
-			'<textarea name="comment">'+comment+'</textarea>' +
+			'<textarea name="comment">' + comment + '</textarea>' +
 			'<input name="markClosed">' +
 			'</form>').submit();
 	});
 
 	//delegate event to document in case of newlly added categories
-	$(document).on("click", ".editable", function() {
+	$(document).on("click", ".editable", function () {
 		$(this).attr('contentEditable', true).focus();
-                $(this).blur(function() {
-                    $(this).attr('contentEditable', false);
-                    var newName = $(this).text();
-                    if (newName){
-                        var id = $(this).parent().attr('id');
-                        var post_data = "newName="+newName+"&category_id="+id;
-                        $.post( "dashboard.php", post_data );
-                    }
-                });
+        $(this).blur(function () {
+            $(this).attr('contentEditable', false);
+            var newName = $(this).text(), id, post_data;
+            if (newName) {
+                id = $(this).parent().attr('id');
+                post_data = "newName=" + newName + "&category_id=" + id;
+                $.post("dashboard.php", post_data);
+            }
+        });
 	});
 
 	//delegate event to document in case of newlly added categories
-	$(document).on("click", ".catRemove", function() {
-		var cat_id = $(this).siblings().attr('id');
-		var post_data = "del_cat_id="+cat_id;
-		$.post( "dashboard.php", post_data );
+	$(document).on("click", ".catRemove", function () {
+		var cat_id = $(this).siblings().attr('id'),
+            post_data = "del_cat_id=" + cat_id;
+		$.post("dashboard.php", post_data);
 		$(this).parent().remove();
 	});
 
-	$('.delUser').click(function() {
+	$('.delUser').click(function () {
 		var uid = $(this).parent().siblings("td.uid").text();
 		$('<form method="post" action="dashboard.php" role="form">' +
 			'<input name="del_user_id" value="' + uid + '">' +
 			'</form>').submit();
 	});
 
-	$(document).on("submit", "#categoryForm", function(e) {
+	$(document).on("submit", "#categoryForm", function (e) {
 		e.preventDefault();
-		var catName = $(this).find('#categoryNameId').val();
-
-		var post_data = $(this).serialize() + "&categorySubmit="+encodeURIComponent(1);
+		var catName = $(this).find('#categoryNameId').val(),
+            post_data = $(this).serialize() + "&categorySubmit=" + encodeURIComponent(1);
 
 		// empty category name field
 		$(this).find('#categoryNameId').val('');
@@ -127,31 +130,32 @@ $(document).ready(function(){
 			return;
 		}
 
-		$.post( "dashboard.php", post_data )
-			.done(function( data ) {
+		$.post("dashboard.php", post_data)
+			.done(function (data) {
 				$('#categories').append(data);
 			});
 	});
 
 
-	$(document).on("click", ".editUser", function() {
+	$(document).on("click", ".editUser", function () {
 	//$('.editUser').click(function() {
-		var username = $(this).parent().siblings("td.name").text();
-		var usermail = $(this).parent().siblings("td.email").text();
-		var userfullname = $(this).parent().siblings("td.fname").text();
-		var userphone = $(this).parent().siblings("td.phone").text();
+		var username = $(this).parent().siblings("td.name").text(),
+            usermail = $(this).parent().siblings("td.email").text(),
+            userfullname = $(this).parent().siblings("td.fname").text(),
+            userphone = $(this).parent().siblings("td.phone").text(),
+            post_data;
 
 		//handle the click of the nav edit for the same user
-		if (!username){
+		if (!username) {
 			username = $(document).find('.user-img').text();
 		}
 		username = username.trim();
 
-		var post_data = { user_name: username, user_email: usermail, user_edit: 1, user_fullname: userfullname, user_phone: userphone };
-		$.post( "edit.php", post_data )
-		  .done(function( data ) {
-			  var content = $( data )[35]; //mou exei spasei ta neura giati to .find('#edit-header') den douleuei
-			  $('#modalUserForm').empty().append(content);
-		  })
+		post_data = { user_name: username, user_email: usermail, user_edit: 1, user_fullname: userfullname, user_phone: userphone };
+		$.post("edit.php", post_data)
+		    .done(function (data) {
+                var content = $(data)[35]; //mou exei spasei ta neura giati to .find('#edit-header') den douleuei
+                $('#modalUserForm').empty().append(content);
+            });
 	});
 });
--- views/js/newreport.js
+++ views/js/newreport.js
@@ -1,4 +1,7 @@
-$(window).load(function() {
+/*jslint browser: true*/
+/*global $, jQuery, alert, google*/
+$(window).load(function () {
+    "use strict";
     function updateMarker(pos) {
         $('#debug').text(pos);
         $('#lat_field').val(pos.k);
@@ -10,29 +13,28 @@ $(window).load(function() {
             center: new google.maps.LatLng(coords.lat, coords.long),
             zoom: 11,
             disableDefaultUI: true
-        };
+        }, map, marker;
 
         $('#debug').text(mapOptions.center);
         $('#lat_field').val(coords.lat);
         $('#lon_field').val(coords.long);
 
-        var map = new google.maps.Map($('#map-container')[0], mapOptions);
-        var marker = new google.maps.Marker({
+        map = new google.maps.Map($('#map-container')[0], mapOptions);
+        marker = new google.maps.Marker({
             draggable: true,
             position: mapOptions.center,
             map: map,
             title: 'Report Position'
         });
 
-        google.maps.event.addListener(marker, 'dragend', function() {
+        google.maps.event.addListener(marker, 'dragend', function () {
             updateMarker(marker.getPosition());
         });
     }
 
     function getCoords(position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        console.log(lat, long);
+        var lat = position.coords.latitude,
+            long = position.coords.longitude;
 
         loadMap({
             lat: lat,
@@ -52,61 +54,58 @@ $(window).load(function() {
         navigator.geolocation.getCurrentPosition(getCoords, geolocationError);
     } else {
         loadMap(greeceLatLng);
-    };
+    }
 
-    $('#add-file-input').click(function() {
+    $('#add-file-input').click(function () {
         var size = $('input[type="file"]').size();
-        if ( size < 4) {
+        if (size < 4) {
             $(this).parent().append('<input class="upload" type="file" accept="image/*;capture=camera" name="images[]">');
         }
     });
 });
 
-$("#ajaxform").submit(function(e)
-{
-    var formObj = $(this);
-    var formURL = formObj.attr("action");
-    var formData = new FormData(this);
+$("#ajaxform").submit(function (e) {
+    "use strict";
+    var formObj = $(this),
+        formURL = formObj.attr("action"),
+        formData = new FormData(this);
     $.ajax({
         url: formURL,
-    type: 'POST',
+        type: 'POST',
         data:  formData,
-    mimeType:"multipart/form-data",
-    contentType: false,
+        mimeType: "multipart/form-data",
+        contentType: false,
         cache: false,
-        processData:false,
-    success: function(data, textStatus, jqXHR)
-    {
-        var content = $( data ).find(".popup");
-        $( "#errors" ).empty().prepend(content);
+        processData: false,
+        success: function (data, textStatus, jqXHR) {
+            var content = $(data).find(".popup");
+            $("#errors").empty().prepend(content);
 
-        content[0].style.opacity = 0;
+            content[0].style.opacity = 0;
 
-        //Make sure the initial state is applied.
-        window.getComputedStyle(content[0]).opacity;
+            //Make sure the initial state is applied.
+            window.getComputedStyle(content[0]).opacity();
 
-        content[0].style.opacity = 1;
+            content[0].style.opacity = 1;
 
-        if ($( data ).find(".popup").hasClass("alert-info")) {
-            document.getElementById("ajaxform").reset();
+            if ($(data).find(".popup").hasClass("alert-info")) {
+                document.getElementById("ajaxform").reset();
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
         }
-
-    },
-     error: function(jqXHR, textStatus, errorThrown)
-     {
-     }
     });
     e.preventDefault(); //STOP default action
 });
 
-	$(document).on("click", ".editUser", function() {
-		username = $(document).find('.user-img').text(); 
-		username = username.trim();
-
-		var post_data = { user_name: username, user_edit: 1 };
-		$.post( "edit.php", post_data )
-		  .done(function( data ) {
-			  var content = $( data )[35]; //mou exei spasei ta neura giati to .find('#edit-header') den douleuei
-			  $('#modalUserForm').empty().append(content);
-		  })
-	});
+$(document).on("click", ".editUser", function () {
+    "use strict";
+    var username = $(document).find('.user-img').text().trim(),
+        post_data = { user_name: username, user_edit: 1 };
+    
+	$.post("edit.php", post_data)
+	    .done(function (data) {
+	        var content = $(data)[35]; //mou exei spasei ta neura giati to .find('#edit-header') den douleuei
+	        $('#modalUserForm').empty().append(content);
+        });
+});
