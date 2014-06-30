4c4f7c0b85f8c90d991ec221bc2340826a947a47
Spyros Seimenis <sespiros@gmail.com>
2014-06-30 03:45:18 +0300

    Linted the py file and modified to work on laptop with corrected category id, den esteila tipota sto mail na ta doume kai aurio to proi last time mpas kai to hostaroume pouthena

--- testbench/FUCKTHESITE.py
+++ testbench/FUCKTHESITE.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 from pymarkovchain import MarkovChain
 from random import uniform
 import os
@@ -8,12 +7,13 @@ import os
 EMAIL = 'sespiros'
 PASSWORD = '123456'
 
-URL = 'http://localhost:8000/reportr/index.php'
-newRep = 'http://localhost:8000/reportr/newreport.php'
+URL = 'http://localhost/reportr/index.php'
+newRep = 'http://localhost/reportr/newreport.php'
+
 
 def main():
-    with open ("test.txt", "r") as myfile:
-      data=myfile.read().replace('\n', '')
+    with open("test.txt", "r") as myfile:
+        data = myfile.read().replace('\n', '')
     mc = MarkovChain("./markovdb")
 
     # Start a session so we can have persistant cookies
@@ -32,26 +32,26 @@ def main():
     mc.generateDatabase(data)
 
     for x in range(0, 5):
-      r = os.urandom(16).encode('hex')
-      title="Report#"+str(x)+" "+str(r)
-      description = mc.generateString()
+        r = os.urandom(16).encode('hex')
+        title = "Report#" + str(x) + " " + str(r)
+        description = mc.generateString()
 
-      #europe only because americans are fags
-      y, x = uniform(-17,43), uniform(28, 55)
+        #europe only because americans are fags
+        y, x = uniform(-17, 43), uniform(28, 55)
 
-      print (title)
+        print (title)
 
-      # Create new report based on random content
-      report_data = {
-          'title': title,
-          'category': "1",
-          'description': description,
-          'latitude': x,
-          'longitude': y,
-          'newreport': "1",
-      }
+        # Create new report based on random content
+        report_data = {
+            'title': title,
+            'category': "2",
+            'description': description,
+            'latitude': x,
+            'longitude': y,
+            'newreport': "1",
+        }
 
-      r = session.post(newRep, data=report_data)
+        r = session.post(newRep, data=report_data)
 
 
 if __name__ == '__main__':
